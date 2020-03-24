b8274067429a05f331600ca9802ef2faf8299ca5
Anthony Besson <besson.anthony@gmail.com>
2020-03-24 17:00:12 +0100

    Versioning

--- src/sources/uci/uci_uci.c
+++ src/sources/uci/uci_uci.c
@@ -17,7 +17,7 @@
 void	uci_uci(const char *args)
 {
 	(void)args;
-	puts("id name Stash v12.0");
+	puts("id name Stash v12.1");
 	puts("id author Morgan Houppin (@mhouppin)");
 	puts("option name Hash type spin default 16 min 1 max 131072");
 	puts("option name Clear Hash type button");
