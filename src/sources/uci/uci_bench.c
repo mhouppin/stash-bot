e9306b85e6a52b882e2c7be3cb7050b8cf466f58
Morgan Houppin <morganhouppin@gmail.com>
2020-06-15 22:17:10 +0200

    Changing bench output to stick to OpenBench format

--- src/sources/uci/uci_bench.c
+++ src/sources/uci/uci_bench.c
@@ -94,8 +94,8 @@ void	uci_bench(const char *args)
 	bench_time = chess_clock() - bench_time;
 
 	printf("Benchmark report:\n");
-	printf("Total time:   %lu milliseconds\n", bench_time);
-	printf("Total nodes:  " SIZE_FORMAT "\n", total_nodes);
-	printf("Nodes/second: " SIZE_FORMAT "\n", (size_t)(((uint64_t)total_nodes * 1000) / (uint64_t)bench_time));
+	printf("Time:  %lu milliseconds\n", bench_time);
+	printf("Nodes: " SIZE_FORMAT "\n", total_nodes);
+	printf("NPS:   " SIZE_FORMAT "\n", (size_t)(((uint64_t)total_nodes * 1000) / (uint64_t)bench_time));
 	fflush(stdout);
 }
