c04b907d39d43e9630c8d1b59797408e3e3431ce
Morgan Houppin <morganhouppin@gmail.com>
2020-12-16 10:07:32 +0100

    Updated CI version to include correct sanitizing tests

--- utils/check_bench.sh
+++
@@ -1,5 +0,0 @@
-_BENCHMARK=$(echo "$1" | fgrep "Bench" | sed "s/Bench: //g")
-echo "Expected benchmark is $_BENCHMARK"
-_OUTPUT=$(./stash bench | fgrep "NODES:" | sed "s/NODES: //g")
-echo "Got $_OUTPUT"
-test "$_BENCHMARK" == "$_OUTPUT" || exit 1
