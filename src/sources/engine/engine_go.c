ad17dbd893f5f85c02c90232f425589cb87ed9bf
Morgan Houppin <morganhouppin@gmail.com>
2020-12-27 23:07:27 +0100

    Retry of lmr_smaller_on_pv with a new tune Bench: 20558508

--- src/sources/engine/engine_go.c
+++ src/sources/engine/engine_go.c
@@ -34,7 +34,7 @@ void        init_reduction_table(void)
 {
     for (int d = 1; d < 64; ++d)
         for (int m = 1; m < 64; ++m)
-            Reductions[d][m] = -0.69 + log(d) * log(m) / 1.17;
+            Reductions[d][m] = 0.07 + log(d) * log(m) / 2.21;
 }
 
 uint64_t    perft(board_t *board, unsigned int depth)
--- src/sources/engine/search.c
+++ src/sources/engine/search.c
@@ -203,7 +203,14 @@ score_t search(board_t *board, int depth, score_t alpha, score_t beta,
 
         // Can we apply LMR ?
         if (depth >= LMR_MinDepth && move_count > LMR_MinMoves && !board->stack->checkers)
-            reduction = max(0, Reductions[min(depth, 63)][min(move_count, 63)]);
+        {
+            reduction = Reductions[min(depth, 63)][min(move_count, 63)];
+
+            // Increase for non-PV nodes
+            reduction += !pv_node;
+
+            reduction = max(reduction, 0);
+        }
         else
             reduction = 0;
 
