ab2f653f793289639b8743898f55c57de50576a8
Morgan Houppin <morganhouppin@gmail.com>
2021-07-03 19:13:44 +0200

    Reduce the margin for FP child nodes by 80cp if improving Bench: 10680727

--- src/sources/search.c
+++ src/sources/search.c
@@ -140,7 +140,7 @@ score_t search(board_t *board, int depth, score_t alpha, score_t beta, searchsta
 
     // Futility Pruning.
 
-    if (!pvNode && depth <= 8 && eval - 80 * depth >= beta && eval < VICTORY)
+    if (!pvNode && depth <= 8 && eval - 80 * (depth - improving) >= beta && eval < VICTORY)
         return (eval);
 
     // Null move pruning.
