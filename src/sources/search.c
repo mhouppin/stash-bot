4ce33b02d24f427b44defad7c9f3c1b25e5950fe
Morgan Houppin <morganhouppin@gmail.com>
2024-03-22 15:24:10 +0100

    Apply extensions to newDepth before LMR
    Bench: 13,131,267

--- src/sources/search.c
+++ src/sources/search.c
@@ -690,6 +690,7 @@ main_loop:
         // it for ordering moves.
         ss->currentMove = currmove;
         ss->pieceHistory = &worker->ctHistory[movedPiece][to_sq(currmove)];
+        newDepth += extension;
 
         do_move_gc(board, currmove, &stack, givesCheck);
         atomic_fetch_add_explicit(&get_worker(board)->nodes, 1, memory_order_relaxed);
@@ -728,14 +729,14 @@ main_loop:
             // Perform another search at full depth if LMR failed high.
             if (r != 0 && score > alpha)
             {
-                score = -search(false, board, newDepth + extension, -alpha - 1, -alpha, ss + 1, !cutNode);
+                score = -search(false, board, newDepth, -alpha - 1, -alpha, ss + 1, !cutNode);
 
                 update_cont_histories(ss, depth, movedPiece, to_sq(currmove), score > alpha);
             }
         }
         // If LMR is not possible, do a search with no reductions.
         else if (!pvNode || moveCount != 1)
-            score = -search(false, board, newDepth + extension, -alpha - 1, -alpha, ss + 1, !cutNode);
+            score = -search(false, board, newDepth, -alpha - 1, -alpha, ss + 1, !cutNode);
 
         // In PV nodes, perform an additional full-window search for the first
         // move, or when all our previous searches returned fail-highs.
