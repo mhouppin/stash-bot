ced26e0d33bd333df4b26c684dbef451d0378bdf
Morgan Houppin <morganhouppin@gmail.com>
2025-02-19 13:24:18 +0100

    Fix the name of the executable file in build scripts
    Bench: 4,123,356

--- utils/build.sh
+++ utils/build.sh
@@ -4,7 +4,7 @@ cd src
 
 make re CFLAGS="-O3 -flto -fprofile-generate" LDFLAGS="-lgcov" ARCH="$ARCH"
 
-./stash-bot bench
+./stash bench
 
 make re CFLAGS="-O3 -flto -fprofile-use -fno-peel-loops -fno-tracer" LDFLAGS="-lgcov" ARCH="$ARCH"
 
