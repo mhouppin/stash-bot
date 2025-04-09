2da654c9a4780d2110e9bd3d87c94d504bde8299
Morgan Houppin <morganhouppin@gmail.com>
2025-04-09 18:33:34 +0200

    Allow reductions to go negative if the move has been extended
    Bench: 4,089,031

--- src/sources/search.c
+++ src/sources/search.c
@@ -895,12 +895,12 @@ main_loop:
 
             // Clamp the reduction so that we don't extend the move or drop
             // immediately into qsearch.
-            r = i16_clamp(r, 0, new_depth - 1);
+            r = i16_clamp(r, -extension, new_depth - 1);
 
             score = -search(false, board, new_depth - r, -alpha - 1, -alpha, ss + 1, true);
 
             // Perform another search at full depth if LMR failed high.
-            if (r != 0 && score > alpha) {
+            if (r > -extension && score > alpha) {
                 score = -search(
                     false,
                     board,
