c570859735a8010fbe6cb609e4faf06c0c75c0a4
Morgan Houppin <morganhouppin@gmail.com>
2025-05-04 22:38:18 +0200

    Improve the reported info if we end up in strong time scramble
    Bench: 4,646,465

--- src/sources/uci.c
+++ src/sources/uci.c
@@ -25,7 +25,7 @@
 #include "wdl.h"
 #include "wmalloc.h"
 
-#define UCI_VERSION "v37.2"
+#define UCI_VERSION "v37.3"
 
 static const Command UciCommands[] = {
     {STATIC_STRVIEW("bench"), uci_bench},
