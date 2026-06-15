0b6a56ba7af3046e5cfae8f13c02458e3782bc3b
Morgan Houppin <morganhouppin@gmail.com>
2026-06-15 17:30:25 +0200

    Try NMP only on expected cutnodes
    Bench: 4,252,962

--- src/sources/search.c
+++ src/sources/search.c
@@ -683,7 +683,7 @@ Score search(
     // the board, we try to see what happens if we skip our turn. If the resulting reduced search
     // still beats beta, we assume our position is so good that we cannot get under beta at this
     // point.
-    if (!pv_node && depth >= 3 && ss->plies >= worker->nmp_verif_plies && !ss->excluded_move
+    if (cut_node && depth >= 3 && ss->plies >= worker->nmp_verif_plies && !ss->excluded_move
         && eval >= beta + 30 && eval >= ss->static_eval
         && board->stack->material[board->side_to_move] != 0) {
         Boardstack stack;
